be29e43fc605ba1837ae3599ef20125c948396d3
Juan José Agudelo Vélez <109243184+Josefo22@users.noreply.github.com>
2024-12-19 22:16:46 -0500

    Update and rename next.config.js to next.config.cjs

---
+++ next.config.cjs
@@ -0,0 +1,7 @@
+// next.config.cjs
+const nextConfig = {
+  reactStrictMode: true,
+  // otras configuraciones
+};
+
+module.exports = nextConfig;
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    // Agrega aquí cualquier configuración adicional que uses
-  };
-  
-  export default nextConfig;
-  
