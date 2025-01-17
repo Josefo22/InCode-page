568038656fe99c60af97c8140ade66cb9106f359
Juan José Agudelo Vélez <josefovelez22@gmail.com>
2025-01-16 19:05:49 -0500

    Descripción de los cambios realizados5

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -5,10 +5,10 @@ import {
   CodeBracketIcon,
   GlobeAltIcon,
   ChartBarIcon,
-} from "@heroicons/react/24/solid";
+} from '@heroicons/react/24/solid';
 
-import Card from "@/components/Card";
-import FloatingWhatsAppButton from "@/components/FloatingWhatsAppButton";
+import Card from '@/components/Card';
+import FloatingWhatsAppButton from '@/components/FloatingWhatsAppButton';
 
 export default function BlogPage() {
   return (
@@ -42,9 +42,9 @@ export default function BlogPage() {
           <Card
             description="Ofrecemos asesoramiento experto para optimizar tus procesos y sistemas mediante tecnología."
             features={[
-              "Análisis de necesidades",
-              "Planificación estratégica",
-              "Optimización de procesos",
+              'Análisis de necesidades',
+              'Planificación estratégica',
+              'Optimización de procesos',
             ]}
             icon={
               <UserGroupIcon className="w-12 h-12 text-blue-500 dark:text-blue-400" />
@@ -55,9 +55,9 @@ export default function BlogPage() {
           <Card
             description="Construimos aplicaciones web personalizadas que se adaptan a las necesidades de tu empresa."
             features={[
-              "Diseño responsivo",
-              "Desarrollo personalizado",
-              "Optimización para SEO",
+              'Diseño responsivo',
+              'Desarrollo personalizado',
+              'Optimización para SEO',
             ]}
             icon={
               <SparklesIcon className="w-12 h-12 text-yellow-500 dark:text-yellow-400" />
@@ -68,9 +68,9 @@ export default function BlogPage() {
           <Card
             description="Brindamos soporte continuo y mantenimiento para asegurar que tus soluciones sigan funcionando al máximo."
             features={[
-              "Actualizaciones regulares",
-              "Corrección de errores",
-              "Optimización de rendimiento",
+              'Actualizaciones regulares',
+              'Corrección de errores',
+              'Optimización de rendimiento',
             ]}
             icon={
               <LightBulbIcon className="w-12 h-12 text-green-500 dark:text-green-400" />
@@ -91,11 +91,11 @@ export default function BlogPage() {
           <Card
             description="Muchas veces una sola categoría no alcanza, quizás necesitás una tienda pero que además tenga integraciones externas, o una web institucional que tenga un catálogo de productos."
             features={[
-              "Carga rápida",
-              "Secciones a pedido",
-              "Maquetado de diseño",
-              "Integraciones con software o apis",
-              "Trabajo en conjunto con equipo",
+              'Carga rápida',
+              'Secciones a pedido',
+              'Maquetado de diseño',
+              'Integraciones con software o apis',
+              'Trabajo en conjunto con equipo',
             ]}
             fullWidth={true}
             icon={
@@ -179,33 +179,33 @@ function FeaturedProjects() {
   const projects = [
     {
       icon: CodeBracketIcon,
-      title: "Proyectos POS",
+      title: 'Proyectos POS',
       description:
-        "Desarrollo de sistemas de punto de venta (POS) completos con gestión de inventarios, ventas y facturación, adaptados a las necesidades del cliente.",
-      bgColor: "bg-blue-50 dark:bg-blue-900/50", // Fondo más sutil
-      textColor: "text-blue-600 dark:text-blue-400", // Texto adaptado a ambos modos
+        'Desarrollo de sistemas de punto de venta (POS) completos con gestión de inventarios, ventas y facturación, adaptados a las necesidades del cliente.',
+      bgColor: 'bg-blue-50 dark:bg-blue-900/50', // Fondo más sutil
+      textColor: 'text-blue-600 dark:text-blue-400', // Texto adaptado a ambos modos
       buttonColor:
-        "bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600",
+        'bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600',
     },
     {
       icon: GlobeAltIcon,
-      title: "Proyectos de IA",
+      title: 'Proyectos de IA',
       description:
-        "Implementación de soluciones de inteligencia artificial para tareas como análisis predictivo, procesamiento de lenguaje natural y automatización.",
-      bgColor: "bg-green-50 dark:bg-green-900/50", // Fondo más sutil
-      textColor: "text-green-600 dark:text-green-400", // Texto adaptado
+        'Implementación de soluciones de inteligencia artificial para tareas como análisis predictivo, procesamiento de lenguaje natural y automatización.',
+      bgColor: 'bg-green-50 dark:bg-green-900/50', // Fondo más sutil
+      textColor: 'text-green-600 dark:text-green-400', // Texto adaptado
       buttonColor:
-        "bg-green-600 hover:bg-green-700 dark:bg-green-500 dark:hover:bg-green-600",
+        'bg-green-600 hover:bg-green-700 dark:bg-green-500 dark:hover:bg-green-600',
     },
     {
       icon: ChartBarIcon,
-      title: "Dashboard en Power BI",
+      title: 'Dashboard en Power BI',
       description:
-        "Creación de dashboards interactivos y personalizados en Power BI para visualizar datos clave y tomar decisiones estratégicas.",
-      bgColor: "bg-purple-50 dark:bg-purple-900/50", // Fondo sutil
-      textColor: "text-purple-600 dark:text-purple-400", // Texto adaptado
+        'Creación de dashboards interactivos y personalizados en Power BI para visualizar datos clave y tomar decisiones estratégicas.',
+      bgColor: 'bg-purple-50 dark:bg-purple-900/50', // Fondo sutil
+      textColor: 'text-purple-600 dark:text-purple-400', // Texto adaptado
       buttonColor:
-        "bg-purple-600 hover:bg-purple-700 dark:bg-purple-500 dark:hover:bg-purple-600",
+        'bg-purple-600 hover:bg-purple-700 dark:bg-purple-500 dark:hover:bg-purple-600',
     },
   ];
 
